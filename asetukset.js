4b7c2fc43e5c9f59f58cdbdb2a53250ee5582a00
Einari Kurvinen <einari.kurvinen@gmail.com>
2016-11-29 07:06:06 +0200

    em -> px, tekstikysymys ennen kuvakysymystä

--- asetukset.js
+++ asetukset.js
@@ -13,8 +13,8 @@
 // Tarkoittaa kysymystä.
 // Arvon pitää olla merkkijono.
 var kysymykset = [ 
-				{optionsList : ['<img src="kuvat/kysymykset/kissa1.jpg" width="150" alt=""/>', '<img src="kuvat/kysymykset/kissa2.jpg" width="150" alt=""/>', '<img src="kuvat/kysymykset/kissa3.jpg" width="150" alt=""/>'], correct : '<img src="kuvat/kysymykset/koira.jpg" width="150" alt=""/>', question : 'Koira'},
 				{optionsList : ['1', '4', '2'], correct : '3', question : '2+1'},				
+				{optionsList : ['<img src="kuvat/kysymykset/kissa1.jpg" width="150" alt=""/>', '<img src="kuvat/kysymykset/kissa2.jpg" width="150" alt=""/>', '<img src="kuvat/kysymykset/kissa3.jpg" width="150" alt=""/>'], correct : '<img src="kuvat/kysymykset/koira.jpg" width="150" alt=""/>', question : 'Koira'},
 				{optionsList : ['3', '5', '1'], correct : '4', question : '2+2'},
 				{optionsList : ['3', '1', '2'], correct : '4', question : '3+1'},		
 				{optionsList : ['3', '2', '0'], correct : '1', question : '0+1'},			
